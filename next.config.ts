6a970ac40146dbedade7d4533a9e72251b6f78c7
mynameisgrass <you@example.com>
2026-08-23 10:35:53 +0700

    Remove next-pwa plugin entirely to fix Vercel Next 15 Turbopack build error

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 import type { NextConfig } from "next";
-import withPWAInit from "@ducanh2912/next-pwa";
-
-const withPWA = withPWAInit({
-  dest: "public",
-  disable: true,
-});
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default withPWA(nextConfig);
+export default nextConfig;
